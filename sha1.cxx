f2e7a0042606fdfbbd36b491f88bf837631b6cda
William McBrine <wmcbrine@gmail.com>
2018-10-25 22:43:21 -0400

    Re-roll the SHA1 transform() loops, move R0-R4 there.

--- sha1.cxx
+++ sha1.cxx
@@ -30,52 +30,13 @@ uint32_t blk(uint32_t *block, int i)
     return block[i & 15];
 }
 
-/* (R0 + R1), R2, R3, R4 are the different operations used in SHA1 */
-
-void R0(uint32_t *block, uint32_t v, uint32_t &w, uint32_t x,
-        uint32_t y, uint32_t &z, int i)
-{
-    z += ((w & (x ^ y)) ^ y) + block[i] + 0x5A827999 + ROTL(v, 5);
-    w = ROTL(w, 30);
-}
-
-void R1(uint32_t *block, uint32_t v, uint32_t &w, uint32_t x,
-        uint32_t y, uint32_t &z, int i)
-{
-    z += ((w & (x ^ y)) ^ y) + blk(block, i) + 0x5A827999 + ROTL(v, 5);
-    w = ROTL(w, 30);
-}
-
-void R2(uint32_t *block, uint32_t v, uint32_t &w, uint32_t x,
-        uint32_t y, uint32_t &z, int i)
-{
-    z += (w ^ x ^ y) + blk(block, i) + 0x6ED9EBA1 + ROTL(v, 5);
-    w = ROTL(w, 30);
-}
-
-void R3(uint32_t *block, uint32_t v, uint32_t &w, uint32_t x,
-        uint32_t y, uint32_t &z, int i)
-{
-    z += (((w | x) & y) | (w & x)) + blk(block, i) + 0x8F1BBCDC + ROTL(v, 5);
-    w = ROTL(w, 30);
-}
-
-void R4(uint32_t *block, uint32_t v, uint32_t &w, uint32_t x,
-        uint32_t y, uint32_t &z, int i)
-{
-    z += (w ^ x ^ y) + blk(block, i) + 0xCA62C1D6 + ROTL(v, 5);
-    w = ROTL(w, 30);
-}
-
 /* Hash a single 512-bit block. This is the core of the algorithm. */
 
 static void transform(uint32_t state[5], const uint8_t buffer[64])
 {
-    uint32_t a, b, c, d, e;
+    uint32_t a, b, c, d, e, tmp;
     uint32_t block[16];
-
-    for (int i = 0; i < 16; i++)
-        block[i] = GET32(&buffer[i << 2]);
+    int i;
 
     /* Copy state[] to working vars */
     a = state[0];
@@ -84,51 +45,37 @@ static void transform(uint32_t state[5], const uint8_t buffer[64])
     d = state[3];
     e = state[4];
 
-    /* 4 rounds of 20 operations each. Loop unrolled. */
-    R0(block, a, b, c, d, e,  0); R0(block, e, a, b, c, d,  1);
-    R0(block, d, e, a, b, c,  2); R0(block, c, d, e, a, b,  3);
-    R0(block, b, c, d, e, a,  4); R0(block, a, b, c, d, e,  5);
-    R0(block, e, a, b, c, d,  6); R0(block, d, e, a, b, c,  7);
-    R0(block, c, d, e, a, b,  8); R0(block, b, c, d, e, a,  9);
-    R0(block, a, b, c, d, e, 10); R0(block, e, a, b, c, d, 11);
-    R0(block, d, e, a, b, c, 12); R0(block, c, d, e, a, b, 13);
-    R0(block, b, c, d, e, a, 14); R0(block, a, b, c, d, e, 15);
+    for (i = 0; i < 16; i++)
+    {
+        block[i] = GET32(&buffer[i << 2]);
 
-    R1(block, e, a, b, c, d, 16); R1(block, d, e, a, b, c, 17);
-    R1(block, c, d, e, a, b, 18); R1(block, b, c, d, e, a, 19);
+        e += ((b & (c ^ d)) ^ d) + block[i] + 0x5A827999;
+        tmp = e + ROTL(a, 5); e = d; d = c; c = ROTL(b, 30); b = a; a = tmp;
+    }
 
-    R2(block, a, b, c, d, e, 20); R2(block, e, a, b, c, d, 21);
-    R2(block, d, e, a, b, c, 22); R2(block, c, d, e, a, b, 23);
-    R2(block, b, c, d, e, a, 24); R2(block, a, b, c, d, e, 25);
-    R2(block, e, a, b, c, d, 26); R2(block, d, e, a, b, c, 27);
-    R2(block, c, d, e, a, b, 28); R2(block, b, c, d, e, a, 29);
-    R2(block, a, b, c, d, e, 30); R2(block, e, a, b, c, d, 31);
-    R2(block, d, e, a, b, c, 32); R2(block, c, d, e, a, b, 33);
-    R2(block, b, c, d, e, a, 34); R2(block, a, b, c, d, e, 35);
-    R2(block, e, a, b, c, d, 36); R2(block, d, e, a, b, c, 37);
-    R2(block, c, d, e, a, b, 38); R2(block, b, c, d, e, a, 39);
+    for ( ; i < 20; i++)
+    {
+        e += ((b & (c ^ d)) ^ d) + blk(block, i) + 0x5A827999;
+        tmp = e + ROTL(a, 5); e = d; d = c; c = ROTL(b, 30); b = a; a = tmp;
+    }
 
-    R3(block, a, b, c, d, e, 40); R3(block, e, a, b, c, d, 41);
-    R3(block, d, e, a, b, c, 42); R3(block, c, d, e, a, b, 43);
-    R3(block, b, c, d, e, a, 44); R3(block, a, b, c, d, e, 45);
-    R3(block, e, a, b, c, d, 46); R3(block, d, e, a, b, c, 47);
-    R3(block, c, d, e, a, b, 48); R3(block, b, c, d, e, a, 49);
-    R3(block, a, b, c, d, e, 50); R3(block, e, a, b, c, d, 51);
-    R3(block, d, e, a, b, c, 52); R3(block, c, d, e, a, b, 53);
-    R3(block, b, c, d, e, a, 54); R3(block, a, b, c, d, e, 55);
-    R3(block, e, a, b, c, d, 56); R3(block, d, e, a, b, c, 57);
-    R3(block, c, d, e, a, b, 58); R3(block, b, c, d, e, a, 59);
+    for ( ; i < 40; i++)
+    {
+        e += (b ^ c ^ d) + blk(block, i) + 0x6ED9EBA1;
+        tmp = e + ROTL(a, 5); e = d; d = c; c = ROTL(b, 30); b = a; a = tmp;
+    }
 
-    R4(block, a, b, c, d, e, 60); R4(block, e, a, b, c, d, 61);
-    R4(block, d, e, a, b, c, 62); R4(block, c, d, e, a, b, 63);
-    R4(block, b, c, d, e, a, 64); R4(block, a, b, c, d, e, 65);
-    R4(block, e, a, b, c, d, 66); R4(block, d, e, a, b, c, 67);
-    R4(block, c, d, e, a, b, 68); R4(block, b, c, d, e, a, 69);
-    R4(block, a, b, c, d, e, 70); R4(block, e, a, b, c, d, 71);
-    R4(block, d, e, a, b, c, 72); R4(block, c, d, e, a, b, 73);
-    R4(block, b, c, d, e, a, 74); R4(block, a, b, c, d, e, 75);
-    R4(block, e, a, b, c, d, 76); R4(block, d, e, a, b, c, 77);
-    R4(block, c, d, e, a, b, 78); R4(block, b, c, d, e, a, 79);
+    for ( ; i < 60; i++)
+    {
+        e += (((b | c) & d) | (b & c)) + blk(block, i) + 0x8F1BBCDC;
+        tmp = e + ROTL(a, 5); e = d; d = c; c = ROTL(b, 30); b = a; a = tmp;
+    }
+
+    for ( ; i < 80; i++)
+    {
+        e += (b ^ c ^ d) + blk(block, i) + 0xCA62C1D6;
+        tmp = e + ROTL(a, 5); e = d; d = c; c = ROTL(b, 30); b = a; a = tmp;
+    }
 
     /* Add the working vars back into context.state[] */
     state[0] += a;
